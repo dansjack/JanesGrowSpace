7ea6d0bdc4fc3f57ef19d455f81f9cf4f3f36233
dsjack <dsjack89@gmail.com>
2019-02-06 12:53:38 -0800

    Removed some unnecesary CSS

--- big/index.php
+++ big/index.php
@@ -16,10 +16,17 @@
   <!-- START RIGHT COL -->
   <aside>
     <h3>Client Resources</h3>
-    <p>List some of your favorite sites that help clients (2 or 3)</p>
     <ul>
       <li>
         <p><a href="https://coolors.co/" target="_blank">coolers.co</a> - Create, save, and share perfect color palettes in seconds!</p>
+      </li>
+      <li>
+        <p><a href="https://randoma11y.com/" target="_blank">randoma11y.com</a> - Find high-contrast color palettes
+          to make your site more inviting to the visually impaired.</p>
+      </li>
+      <li>
+        <p><a href="https://www.typography.com/techniques/" target="_blank">Fonts by Hoefler&Co: Technqiues</a> - A
+          guide for choosing coherent font combinations.</p>
       </li>
     </ul>
   </aside>
