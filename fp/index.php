988ddd5cc7b1eec6c7f8e0a38734bd3856bde350
dsjack <dsjack89@gmail.com>
2019-02-15 14:27:28 -0800

    Resolves sticky nav alignment after scrolling

--- fp/index.php
+++ fp/index.php
@@ -5,19 +5,16 @@
   <meta name="viewport" content="width=device-width" />
   <meta name="robots" content="noindex,nofollow" />
   <meta charset="utf-8">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
   <link rel="stylesheet" href="css/nav.css" />
   <link rel="stylesheet" href="css/fp.css" />
   <link rel="stylesheet" href="css/form.css" />
-
   <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates|Ubuntu" rel="stylesheet">
 </head>
 
 <body>
 <!-- START WRAPPER -->
 <div class="wrapper">
-  <div class="section">
+  <div class="section group">
     <header class="col span_6_of_6">
         <h1><a href="index.php">Jane's grow space</a></h1>
         <nav id="navbar">
@@ -32,7 +29,7 @@
 
   <!--  end header file here -->
   <!-- START LEFT COL -->
-  <div class="section" id="pageID">
+  <div class="section group">
     <h2 class="col span_6_of_6">Welcome to the Jungle...</h2>
   </div>
   <div class="section group">
@@ -47,7 +44,9 @@
     <!-- START RIGHT COL -->
     <main class="col span_4_of_6">
       <h2>Main</h2>
-      <p>Stare at ceiling attack the child or murr i hate humans they are so annoying eat the rubberband. Asdflkjaertvlkjasntvkjn (sits on keyboard) soft kitty warm kitty little ball of furr, step on your keyboard while you're gaming and then turn in a circle twitch tail in permanent irritation yet poop on floor and watch human clean up pushes butt to face somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock. do not try to mix old food with new one to fool me! meeeeouw hopped up on catnip use lap as chair. Roll over and sun my belly attack dog, run away and pretend to be victim or cry louder at reflection. Yowling nonstop the whole night pushes butt to face kitty power. Sleep nap crash against wall but walk away like nothing happened and paw your face to wake you up in the morning, meow in empty rooms yet bird bird bird bird bird bird human why take bird out i could have eaten that but bite the neighbor's bratty kid. Ccccccccccccaaaaaaaaaaaaaaatttttttttttttttttssssssssssssssss find something else more interesting, for hiding behind the couch until lured out by a feathery toy that box? i can fit in that box groom forever, stretch tongue and leave it slightly out, blep or intently sniff hand spill litter box, scratch at owner, destroy all furniture, especially couch. Sleeps on my head ooooh feather moving feather!, and chase red laser dot if human is on laptop sit on the keyboard. If it smells like fish eat as much as you wish meowzer for scratch my tummy actually i hate you now fight me, so spill litter box, scratch at owner, destroy all furniture, especially couch, hide when guests come over. Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life i am the best play riveting piece on synthesizer keyboard and sleep in the bathroom sink. Be superior stare at guinea pigs touch my tail, i shred your hand purrrr so sit on human they not getting up ever i love cuddles or then cats take over the world for bleghbleghvomit my furball really tie the room together.
+      <p>Stare at ceiling attack the child or murr i hate humans they are so annoying eat the rubberband.
+        Asdflkjaertvlkjasntvkjn (sits on keyboard) soft kitty warm kitty little ball of furr, step on your keyboard
+        while you're gaming and then turn in a circle twitch tail in permanent irritation yet poop on floor and watch human clean up pushes butt to face somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock. do not try to mix old food with new one to fool me! meeeeouw hopped up on catnip use lap as chair. Roll over and sun my belly attack dog, run away and pretend to be victim or cry louder at reflection. Yowling nonstop the whole night pushes butt to face kitty power. Sleep nap crash against wall but walk away like nothing happened and paw your face to wake you up in the morning, meow in empty rooms yet bird bird bird bird bird bird human why take bird out i could have eaten that but bite the neighbor's bratty kid. Find something else more interesting, for hiding behind the couch until lured out by a feathery toy that box? i can fit in that box groom forever, stretch tongue and leave it slightly out, blep or intently sniff hand spill litter box, scratch at owner, destroy all furniture, especially couch. Sleeps on my head ooooh feather moving feather!, and chase red laser dot if human is on laptop sit on the keyboard. If it smells like fish eat as much as you wish meowzer for scratch my tummy actually i hate you now fight me, so spill litter box, scratch at owner, destroy all furniture, especially couch, hide when guests come over. Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life i am the best play riveting piece on synthesizer keyboard and sleep in the bathroom sink. Be superior stare at guinea pigs touch my tail, i shred your hand purrrr so sit on human they not getting up ever i love cuddles or then cats take over the world for bleghbleghvomit my furball really tie the room together.
         Toy mouse squeak roll over. Cats woo i cry and cry and cry unless you pet me, and then maybe i cry just for fun but mice, or lies down tuxedo cats always looking dapper but milk the cow. Hopped up on catnip swat turds around the house yet refuse to leave cardboard box yet spit up on light gray carpet instead of adjacent linoleum for cat dog hate mouse eat string barf pillow no baths hate everything. Lick master's hand at first then bite because im moody check cat door for ambush 10 times before coming in. Scratch leg; meow for can opener to feed me stand with legs in litter box, but poop outside for trip on catnip or i could pee on this if i had the energy or push your water glass on the floor yet twitch tail in permanent irritation but sleep on my human's head. Cats go for world domination scoot butt on the rug and howl uncontrollably for no reason. </p>
     </main>
   </div>
