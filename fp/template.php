be1e8d2bb4dec318605e5fb957aeea98d9a488af
dsjack <dsjack89@gmail.com>
2019-02-21 11:40:35 -0800

    Adds 2 stock photos to Main section
    Also changes fonts family and gives the responsive nav function a proper name.

--- fp/template.php
+++ fp/template.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="css/fp.css" />
   <link rel="stylesheet" href="css/form.css" />
   <link rel="stylesheet" href="css/grid.css" />
-  <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates%7CUbuntu" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates%7CUbuntu%7COswald" rel="stylesheet">
 </head>
 
 <body>
@@ -41,7 +41,20 @@
     <aside class="col span_2_of_6">
       <h3>Aside</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique risus nec feugiat in fermentum posuere urna nec tincidunt. Et netus et malesuada fames ac turpis. Dictum fusce ut placerat orci. Morbi quis commodo odio aenean sed adipiscing diam. Elit eget gravida cum sociis natoque. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Sed velit dignissim sodales ut eu sem. Nunc sed augue lacus viverra vitae congue eu. Est ante in nibh mauris. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Consequat mauris nunc congue nisi vitae suscipit tellus. Sed blandit libero volutpat sed cras. Nulla at volutpat diam ut venenatis tellus in metus vulputate.      </p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Tristique risus nec feugiat in fermentum posuere urna nec tincidunt. Et netus et
+        malesuada fames ac turpis. Dictum fusce ut placerat orci. Morbi quis commodo odio aenean sed adipiscing diam
+        . Elit eget gravida cum sociis natoque. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Sed velit
+        dignissim sodales ut eu sem. Nunc sed augue lacus viverra vitae congue eu. Est ante in nibh mauris. Eleifend
+        mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Eget nulla facilisi etiam dignissim diam
+        quis enim lobortis. Consequat mauris nunc congue nisi vitae suscipit tellus. Sed blandit libero volutpat sed
+        cras. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Cats woo i cry and cry and cry unless
+        you pet me, and then maybe i cry just for fun but mice, or lies down tuxedo cats always looking dapper but
+        milk the cow. Hopped up on catnip swat turds around the house yet refuse to leave cardboard box yet spit up
+        on light gray carpet instead of adjacent linoleum for cat dog hate mouse eat string barf pillow no baths
+        hate everything. Lick master's hand at first then bite because im moody check cat door for ambush 10 times
+        before coming in.
+        </p>
     </aside>
     <!-- END LEFT COL -->
 
@@ -50,13 +63,34 @@
       <h2>Main</h2>
       <p>Stare at ceiling attack the child or murr i hate humans they are so annoying eat the rubberband.
         Asdflkjaertvlkjasntvkjn (sits on keyboard) soft kitty warm kitty little ball of furr, step on your keyboard
-        while you're gaming and then turn in a circle twitch tail in permanent irritation yet poop on floor and watch human clean up pushes butt to face somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock. do not try to mix old food with new one to fool me! meeeeouw hopped up on catnip use lap as chair. Roll over and sun my belly attack dog, run away and pretend to be victim or cry louder at reflection. Yowling nonstop the whole night pushes butt to face kitty power. Sleep nap crash against wall but walk away like nothing happened and paw your face to wake you up in the morning, meow in empty rooms yet bird bird bird bird bird bird human why take bird out i could have eaten that but bite the neighbor's bratty kid. Find something else more interesting, for hiding behind the couch until lured out by a feathery toy that box? i can fit in that box groom forever, stretch tongue and leave it slightly out, blep or intently sniff hand spill litter box, scratch at owner, destroy all furniture, especially couch. Sleeps on my head ooooh feather moving feather!, and chase red laser dot if human is on laptop sit on the keyboard. If it smells like fish eat as much as you wish meowzer for scratch my tummy actually i hate you now fight me, so spill litter box, scratch at owner, destroy all furniture, especially couch, hide when guests come over. Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life i am the best play riveting piece on synthesizer keyboard and sleep in the bathroom sink. Be superior stare at guinea pigs touch my tail, i shred your hand purrrr so sit on human they not getting up ever i love cuddles or then cats take over the world for bleghbleghvomit my furball really tie the room together.
-        Toy mouse squeak roll over. Cats woo i cry and cry and cry unless you pet me, and then maybe i cry just for fun but mice, or lies down tuxedo cats always looking dapper but milk the cow. Hopped up on catnip swat turds around the house yet refuse to leave cardboard box yet spit up on light gray carpet instead of adjacent linoleum for cat dog hate mouse eat string barf pillow no baths hate everything. Lick master's hand at first then bite because im moody check cat door for ambush 10 times before coming in. Scratch leg; meow for can opener to feed me stand with legs in litter box, but poop outside for trip on catnip or i could pee on this if i had the energy or push your water glass on the floor yet twitch tail in permanent irritation but sleep on my human's head. Cats go for world domination scoot butt on the rug and howl uncontrollably for no reason. </p>
+        while you're gaming and then turn in a circle twitch tail in permanent irritation yet poop on floor and
+        watch human clean up pushes butt to face somehow manage to catch a bird but have no idea what to do next, so
+        play with it until it dies of shock. do not try to mix old food with new one to fool me! meeeeouw hopped up
+        on catnip use lap as chair. Roll over and sun my belly attack dog, run away and pretend to be victim or cry
+        louder at reflection. Yowling nonstop the whole night pushes butt to face kitty power. Sleep nap crash
+        against wall but walk away like nothing happened and paw your face to wake you up in the morning, meow in
+        empty rooms yet bird bird bird bird bird bird human why take bird out i could have eaten that but bite the
+        neighbor's bratty kid. Find something else more interesting, for hiding behind the couch until lured out by
+        a feathery toy that box? i can fit in that box groom forever, stretch tongue and leave it slightly out, blep
+        or intently sniff hand spill litter box, scratch at owner, destroy all furniture, especially couch. Sleeps
+        on my head ooooh feather moving feather!, and chase red laser dot if human is on laptop sit on the keyboard.
+        If it smells like fish eat as much as you wish meowzer for scratch my tummy actually i hate you now fight
+        me, so spill litter box, scratch at owner, destroy all furniture, especially couch, hide when guests come
+        over. Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in
+        luxury cat life i am the best play riveting piece on synthesizer keyboard and sleep in the bathroom sink. Be
+        superior stare at guinea pigs touch my tail, i shred your hand purrrr so sit on human they not getting up
+        ever i love cuddles or then cats take over the world for bleghbleghvomit my furball really tie the room
+        together. Toy mouse squeak roll over.
+      </p>
+      <img class="main-img-1" src="images/stock/plants-stock-1.jpg" alt="plant-stock-1">
+      <img class="main-img-2" src="images/stock/plants-stock-2.jpg" alt="plant-stock-2">
     </main>
   </div>
   <!-- END RIGHT COL -->
   <!--START FOOTER HERE-->
   <footer>
+    <p><a href="mailto:janey.exe@gmail.com">janey.exe@gmail.com</a></p>
+    <p><a href="https://www.instagram.com/janeyenaj/?hl=en">Instagram</a></p>
     <p>
       <small>&copy; <?php echo date("Y");?> by <a href="contact.php" target="_blank">Janey Jack</a>, All Rights
         Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
