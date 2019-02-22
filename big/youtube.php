9d2a65aedfcd9a3dc6cc999f991eda8571eb0571
dsjack <dsjack89@gmail.com>
2019-02-22 14:41:05 -0800

    Adds YouTube video to BIG

---
+++ big/youtube.php
@@ -0,0 +1,32 @@
+<?PHP include 'includes/config.php';?>
+<?PHP include 'includes/header.php';?>
+
+    <!-- START LEFT COL -->
+    <section>
+        <h2 class="pageID">YouTube</h2>
+        <div class="embed-container">
+            <iframe src='https://www.youtube.com/embed/x18vEEfpK3g' frameborder='0' allowfullscreen></iframe>
+        </div>
+    </section>
+    <!-- END LEFT COL -->
+
+    <!-- START RIGHT COL -->
+    <aside>
+        <h3>Client Resources</h3>
+        <ul>
+            <li>
+                <p><a href="https://coolors.co/" target="_blank">coolers.co</a> - Create, save, and share perfect color palettes in seconds!</p>
+            </li>
+            <li>
+                <p><a href="https://randoma11y.com/" target="_blank">randoma11y.com</a> - Find high-contrast color palettes
+                    to make your site more inviting to the visually impaired.</p>
+            </li>
+            <li>
+                <p><a href="https://www.typography.com/techniques/" target="_blank">Fonts by Hoefler&Co: Technqiues</a> - A
+                    guide for choosing coherent font combinations.</p>
+            </li>
+        </ul>
+    </aside>
+    <!-- END RIGHT COL -->
+    <!--START FOOTER HERE-->
+<?PHP include 'includes/footer.php';?>
