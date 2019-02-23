f7ddbbfc98a38a8c54cdcf106f488d687d269eaf
dsjack <dsjack89@gmail.com>
2019-02-23 13:43:39 -0800

    Adds Google Map to BIG

---
+++ big/map.php
@@ -0,0 +1,32 @@
+<?PHP include 'includes/config.php';?>
+<?PHP include 'includes/header.php';?>
+
+    <!-- START LEFT COL -->
+    <section>
+        <h2 class="pageID">Map to Seattle Central College</h2>
+        <div class="embed-container">
+            <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.524763499405!2d-122
+             .32369194852727!3d47.61592977908323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13
+              .1!3m3!1m2!1s0x54906accc351c149%3A0xdc1a5c338dd4395c!2sSeattle+Central+College!5e0!3m2!1sen!2sus
+               !4v1550955821248' width='600' height='450' frameborder='0' style='border:0' allowfullscreen></iframe>
+        </div>
+    </section>
+    <!-- END LEFT COL -->
+
+    <!-- START RIGHT COL -->
+    <aside class="map-aside">
+        <h3>Contact Info</h3>
+        <address>
+            1701 Broadway<br />
+            Seattle, WA 98122<br  />
+            <a href="tel:12069343800">(206) 934-3800</a>
+        </address>
+
+    </aside>
+    <!-- END RIGHT COL -->
+    <!--START FOOTER HERE-->
+<?PHP include 'includes/footer.php';?>
+
+<!--Seattle Central College-->
+<!--1701 Broadway-->
+<!--Seattle, WA 98122-->
