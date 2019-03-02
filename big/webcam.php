dec36d60cf0236b399d13ba4faf1aae34aab2aad
dsjack <dsjack89@gmail.com>
2019-03-02 14:58:21 -0800

    Adds web cams to BIG site.
    There is a scroll bar on the traffic cam

--- big/webcam.php
+++ big/webcam.php
@@ -3,32 +3,19 @@
 
     <!-- START LEFT COL -->
     <section>
-        <h2 class="pageID">Web Cams</h2>
+        <h2 class="pageID">Live Web Cams</h2>
         <div class="embed-container">
-            <iframe src='https://www.youtube.com/embed/x18vEEfpK3g' allowfullscreen></iframe>
+          <iframe src='https://www.youtube.com/embed/4993sBLAzGA' allowfullscreen></iframe>
         </div>
     </section>
     <!-- END LEFT COL -->
 
     <!-- START RIGHT COL -->
     <aside>
-        <h3>Client Resources</h3>
-        <ul>
-            <li>
-                <p><a href="https://www.dreamhost.com/blog/make-your-website-accessible/" target="_blank">dreamhost
-                        .com blog: </a> - 10 Ways to Make Your Website Accessible</p>
-            </li>
-            <li>
-                <p><a href="https://webaccess.berkeley.edu/resources/tips/web-accessibility" target="_blank">UC
-                        Berkeley</a> - Top 10 Tips for Making Your Website Accessible</p>
-            </li>
-            <li>
-                <p><a href="https://achecker.ca/checker/index.php" target="_blank">AChecker.com</a> - This tool
-                    checks single HTML pages for conformance with accessibility standards to ensure the content can
-                    be accessed by everyone. See the Handbook link to the upper right for more about the Web
-                    Accessibility Checker.</p>
-            </li>
-        </ul>
+        <h3>WSDOT Camera</h3>
+        <div class="embed-container dot">
+          <iframe src='http://danjack.dreamhosters.com/web120/big/webcam.html'></iframe>
+        </div>
     </aside>
     <!-- END RIGHT COL -->
     <!--START FOOTER HERE-->
